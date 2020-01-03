786fc34fa25d2c474430ac298ec4420690f59afa
Your Name <you@example.com>
2020-01-03 09:19:28 +0800

    adsa

--- src/main/java/cn/wzvtc/soft/TestController.java
+++ src/main/java/cn/wzvtc/soft/TestController.java
@@ -1,22 +1,53 @@
 package cn.wzvtc.soft;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-//@CrossOrigin
+@CrossOrigin(origins = {"http://127.0.0.1:5500"})
 @RestController("/" )
 public class TestController {
+    @Autowired
+    LvliRepository lvliRepository;
 
-    @RequestMapping(value="/data.json",method={RequestMethod.GET})
-    public Map<String,String> bookById(){
+    @RequestMapping(value = "/addlvli")
+    public void addlvli(String lvli){
+        Lvli lvli1=new Lvli("1202",lvli);
+        this.lvliRepository.save(lvli1);
+    }
+    @RequestMapping(value = "/lvlilist")
+    public List<Lvli> getlvlilist(){
+
+        return this.lvliRepository.findAll();
+    }
+    @RequestMapping(value="/userinfo")
+    public Map<String,String> userinfo(HttpSession httpSession){
         Map<String,String> resultMap=new HashMap<>();
-        resultMap.put("myname","连加龙");
-        resultMap.put("mynumber","18002090218");
+        if(httpSession.getAttribute("loginnumber")!=null){
+            resultMap.put("myname","连加龙");
+            resultMap.put("mynumber","18002090218");
+        }
+
         return resultMap;
+    }
+    @RequestMapping(value="/login")
+    public Map<String,String> login(@RequestBody Map<String,String> map, HttpSession httpSession){
+       // Map<String,String> resultMap=new HashMap<>();
+        String password=map.get("password");
+        String number=map.get("number");
+        Map<String,String> resultMap=new HashMap<>();
+       if("18002090218".equals(number)&&"18002090218".equals(password)){
+           httpSession.setAttribute("loginnumber",number);
+           resultMap.put("result","success");
+       }
+        return resultMap;
+    }
+    @RequestMapping(value="/logout")
+    public void logout(HttpServletRequest httpServletRequest){
+        httpServletRequest.getSession().removeAttribute("loginnumber");
     }
 }
